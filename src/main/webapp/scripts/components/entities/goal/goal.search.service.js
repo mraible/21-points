398e6c675533c63d81c6bc8adffe1de88c24008b
Matt Raible <matt@raibledesigns.com>
2015-08-18 12:04:34 -0600

    Removed old entities since I messed something up with git reset.

--- src/main/webapp/scripts/components/entities/goal/goal.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('21pointsApp')
-    .factory('GoalSearch', function ($resource) {
-        return $resource('api/_search/goals/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
--- src/main/webapp/scripts/components/entities/metric/metric.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('21pointsApp')
-    .factory('MetricSearch', function ($resource) {
-        return $resource('api/_search/metrics/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
