f1a1e9da1b85f42419313123a35ca8bd1e8fbadb
mraible <matt@raibledesigns.com>
2015-10-21 15:43:32 -0600

    One-command build and deploy.

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#./gradlew -Pprod bootRepackage -x test
+./gradlew clean -Pprod bootRepackage -x test
 heroku deploy:jar --jar build/libs/*.war --includes newrelic.jar:newrelic.yml
