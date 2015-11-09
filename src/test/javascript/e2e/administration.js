72779fa88be22c78fb19571f4e6f4d7018be361a
Matt Raible <matt@raibledesigns.com>
2015-11-08 22:37:42 -0700

    #8: Fixed names of tests.

--- src/test/javascript/e2e/administration.js
+++ src/test/javascript/e2e/administration.js
@@ -27,27 +27,27 @@ describe('administration', function () {
         expect(element.all(by.css('h2')).first().getText()).toMatch(/Application Metrics/);
     });
 
-    it('should load metrics', function () {
+    it('should load health', function () {
         element(by.css('[ui-sref="health"]')).click();
         expect(element.all(by.css('h2')).first().getText()).toMatch(/Health checks/);
     });
 
-    it('should load metrics', function () {
+    it('should load configuration', function () {
         element(by.css('[ui-sref="configuration"]')).click();
         expect(element.all(by.css('h2')).first().getText()).toMatch(/Configuration/);
     });
 
-    it('should load metrics', function () {
+    it('should load audits', function () {
         element(by.css('[ui-sref="audits"]')).click();
         expect(element.all(by.css('h2')).first().getText()).toMatch(/Audits/);
     });
 
-    it('should load metrics', function () {
+    it('should load logs', function () {
         element(by.css('[ui-sref="logs"]')).click();
         expect(element.all(by.css('h2')).first().getText()).toMatch(/Logs/);
     });
 
-    it('should load metrics', function () {
+    it('should load api docs', function () {
         element(by.css('[ui-sref="docs"]')).click();
         expect(element.all(by.css('iframe')).isDisplayed()).toBeTruthy();
     });
