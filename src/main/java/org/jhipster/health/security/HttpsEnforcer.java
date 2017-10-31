45edf21beb91e16d4084bd7933784664b5fd96fd
Matt Raible <matt.raible@okta.com>
2017-10-31 08:35:37 -0600

    Formatting

--- src/main/java/org/jhipster/health/security/HttpsEnforcer.java
+++ src/main/java/org/jhipster/health/security/HttpsEnforcer.java
@@ -17,7 +17,8 @@ public class HttpsEnforcer implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
 
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
