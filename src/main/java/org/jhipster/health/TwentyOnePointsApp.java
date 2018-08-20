e39c1271b1a1280b977e56faaf3d4ae95868bf71
Matt Raible <matt.raible@okta.com>
2018-08-19 21:46:37 -0600

    Got Jest working locally with embedded elasticsearch

--- src/main/java/org/jhipster/health/TwentyOnePointsApp.java
+++ src/main/java/org/jhipster/health/TwentyOnePointsApp.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
@@ -18,7 +20,7 @@ import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.Collection;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 public class TwentyOnePointsApp {
 
