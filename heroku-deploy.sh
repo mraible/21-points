0597b8d1adb638536e8310db019150dc3a7153ca
Matt Raible <matt.raible@okta.com>
2018-10-13 19:33:10 -0600

    Change Gradle command to bootWar

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-./gradlew clean -Pprod bootRepackage -x test
+./gradlew clean -Pprod bootWar -x test
 #heroku deploy:jar --jar build/libs/*.war --includes newrelic.jar:newrelic.yml
 heroku deploy:jar --jar build/libs/*.war
