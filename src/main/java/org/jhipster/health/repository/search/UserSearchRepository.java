8546c821217efeb383395a90a6c343045f838c42
Matt Raible <matt.raible@okta.com>
2019-03-30 14:28:13 -0700

    Change long to UUID in Elasticsearch

--- src/main/java/org/jhipster/health/repository/search/UserSearchRepository.java
+++ src/main/java/org/jhipster/health/repository/search/UserSearchRepository.java
@@ -3,8 +3,10 @@ package org.jhipster.health.repository.search;
 import org.jhipster.health.domain.User;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.UUID;
+
 /**
  * Spring Data Elasticsearch repository for the User entity.
  */
-public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+public interface UserSearchRepository extends ElasticsearchRepository<User, UUID> {
 }
