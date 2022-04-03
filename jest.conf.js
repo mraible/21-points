cbc689922311f52ca1ea02698be4bbe03804f032
Matt Raible <matt.raible@okta.com>
2022-04-03 10:25:59 -0600

    Initial version of TwentyOnePoints generated by generator-jhipster@7.8.0

---
+++ jest.conf.js
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+
+const {
+  compilerOptions: { paths = {}, baseUrl = './' },
+} = require('./tsconfig.json');
+const environment = require('./webpack/environment');
+
+module.exports = {
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|dayjs/esm)'],
+  resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
+  globals: {
+    ...environment,
+  },
+  roots: ['<rootDir>', `<rootDir>/${baseUrl}`],
+  modulePaths: [`<rootDir>/${baseUrl}`],
+  setupFiles: ['jest-date-mock'],
+  cacheDirectory: '<rootDir>/build/jest-cache',
+  coverageDirectory: '<rootDir>/build/test-results/',
+  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: `<rootDir>/${baseUrl}/` }),
+  reporters: ['default', ['jest-junit', { outputDirectory: '<rootDir>/build/test-results/', outputName: 'TESTS-results-jest.xml' }]],
+  testResultsProcessor: 'jest-sonar-reporter',
+  testMatch: ['<rootDir>/src/main/webapp/app/**/@(*.)@(spec.ts)'],
+  testURL: 'http://localhost/',
+};
---
+++ src/test/java/org/jhipster/health/config/NoOpMailConfiguration.java
@@ -0,0 +1,25 @@
+package org.jhipster.health.config;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
+import org.jhipster.health.service.MailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NoOpMailConfiguration {
+
+    private final MailService mockMailService;
+
+    public NoOpMailConfiguration() {
+        mockMailService = mock(MailService.class);
+        doNothing().when(mockMailService).sendActivationEmail(any());
+    }
+
+    @Bean
+    public MailService mailService() {
+        return mockMailService;
+    }
+}
