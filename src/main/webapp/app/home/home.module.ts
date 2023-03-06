816924f5b7c5b401f7bb398507071a8a4b66e9a6
Matt Raible <matt@raibledesigns.com>
2023-03-06 11:16:56 -0700

    Add Angular Calendar

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,15 +1,28 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
-import { HOME_ROUTE } from './home.route';
+import { ABOUT_ROUTE, HISTORY_ROUTE, HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 import { NgChartsModule } from 'ng2-charts';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AboutComponent } from '../about/about.component';
 import { HistoryComponent } from '../history/history.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), NgChartsModule],
+  imports: [
+    SharedModule,
+    NgChartsModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    RouterModule.forChild([HOME_ROUTE, ABOUT_ROUTE, HISTORY_ROUTE]),
+  ],
   declarations: [HomeComponent, AboutComponent, HistoryComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeModule {}
