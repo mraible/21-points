65ed03c491b7e08398bd96d3811cfaa16905f988
Matt Raible <matt@raibledesigns.com>
2023-04-22 17:32:19 -0600

    Polishing

--- src/main/java/org/jhipster/health/config/ApplicationProperties.java
+++ src/main/java/org/jhipster/health/config/ApplicationProperties.java
@@ -3,7 +3,7 @@ package org.jhipster.health.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Properties specific to Twenty One Points.
+ * Properties specific to 21-Points Health.
  * <p>
  * Properties are configured in the {@code application.yml} file.
  * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
