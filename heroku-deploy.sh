d192cd8f1feda802ed057a703d7ed5e7fdccab78
Matt Raible <matt@raibledesigns.com>
2023-04-27 21:08:12 -0600

    Release 7.0.0

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -5,5 +5,8 @@ set -e
 
 # change the versions in package.json and build.gradle and release before running.
 ./gradlew clean -Pprod bootJar -x test
-#heroku deploy:jar --jar build/libs/*.jar --includes newrelic.jar:newrelic.yml
-heroku deploy:jar --jar build/libs/*.jar
+
+# If the command below doesn't work, run heroku plugins:install java
+heroku deploy:jar --jar build/libs/*.jar \
+  --includes newrelic-agent.jar:newrelic.yml \
+  --app health-by-points
